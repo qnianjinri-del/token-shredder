af01db53fb763a70116e4734fb3c93bfabf5d269
qnianjinri-del <192697308+qnianjinri-del@users.noreply.github.com>
2026-06-11 19:23:10 +0800

    Ignore release tooling in lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,17 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'dist-electron', 'node_modules'] },
+  {
+    ignores: [
+      'coverage',
+      'dist',
+      'dist-electron',
+      'node_modules',
+      'release',
+      'tmp',
+      '*.tsbuildinfo',
+    ],
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
